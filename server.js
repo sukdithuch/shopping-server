c3c4b8d51756c8ccc49a9b642e7e053d8baa2c25
sukdithuch <sukdithuch_y@hotmail.com>
2021-06-18 15:33:58 +0700

    fix cors 5

--- server.js
+++ server.js
@@ -6,23 +6,23 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3006;
 
-var whitelist=['https://shopping-s.herokuapp.com', 'https://shopping-server-sql.herokuapp.com']; //white list consumers
-var corsOptions={
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !==-1) {
-      callback(null, true);
-    } else {
-      callback(null, false);
-    }
-  },
-  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
-};
+// var whitelist=['https://shopping-s.herokuapp.com', 'https://shopping-server-sql.herokuapp.com']; //white list consumers
+// var corsOptions={
+//   origin: function (origin, callback) {
+//     if (whitelist.indexOf(origin) !==-1) {
+//       callback(null, true);
+//     } else {
+//       callback(null, false);
+//     }
+//   },
+//   methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'device-remember-token', 'Access-Control-Allow-Origin', 'Origin', 'Accept']
+// };
 
 app.use(express.json());
-app.use(cors(corsOptions
+app.use(cors());
     
 //     {
 //     origin: ["*"],
@@ -31,7 +31,7 @@ app.use(cors(corsOptions
 //     credentials: true,
 // }
 
-));
+// ));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = mysql.createConnection({
